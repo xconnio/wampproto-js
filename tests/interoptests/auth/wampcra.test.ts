69d6bd0863248b34b01be42089594e13634e131a
Muzzammil Shahid <muzzammil@xconn.io>
2025-04-07 16:27:54 +0500

    Fix CRA authenticate to include salt

--- tests/interoptests/auth/wampcra.test.ts
+++ tests/interoptests/auth/wampcra.test.ts
@@ -1,12 +1,30 @@
-import * as wampCRAAuth from '../../../lib/auth/wampcra'
+import * as wampCRAAuth from "../../../lib/auth/wampcra";
 import {runCommand} from "../helpers";
+import {Challenge, ChallengeFields} from "../../../lib/messages/challenge";
+import {WAMPCRAAuthenticator} from "../../../lib/auth/wampcra";
 
+describe("WAMP CRA Tests", function () {
+    const SESSION_ID = 123;
+    const AUTH_ID = "foo";
+    const AUTH_ROLE = "admin";
+    const PROVIDER = "provider";
+    const TEST_SECRET = "secret";
+    const SALT = "salt";
+    const KEY_LENGTH = 32;
+    const ITERATIONS = 1000;
+    const CRA_CHALLENGE =
+        '{"nonce":"cdcb3b12d56e12825be99f38f55ba43f","authprovider":"provider",' +
+        '"authid":"foo","authrole":"admin","authmethod":"wampcra","session":123,"timestamp":"2024-05-07T09:25:13.307Z"}';
 
-describe('WAMP CRA Tests', function () {
-    const TEST_SECRET: string = "private";
+    const AUTH_EXTRA = {
+        challenge: CRA_CHALLENGE,
+        salt: SALT,
+        iterations: ITERATIONS,
+        keylen: KEY_LENGTH,
+    };
 
     it('Generate challenge', async function () {
-        const challenge: string = wampCRAAuth.generateWAMPCRAChallenge(1, "foo", "admin", "static");
+        const challenge: string = wampCRAAuth.generateWAMPCRAChallenge(SESSION_ID, AUTH_ID, AUTH_ROLE, PROVIDER);
 
         const signature: string = await runCommand(
             `wampproto auth cra sign-challenge ${challenge} ${TEST_SECRET}`
@@ -17,8 +35,10 @@ describe('WAMP CRA Tests', function () {
         );
     });
 
-    it('Sign CRA Challenge', async function() {
-        const challenge: string = await runCommand('wampproto auth cra generate-challenge 1 foo admin static');
+    it('Sign CRA Challenge', async function () {
+        const challenge: string = await runCommand(
+            `wampproto auth cra generate-challenge ${SESSION_ID} ${AUTH_ID} ${AUTH_ROLE} ${PROVIDER}`
+        );
 
         const signature: string = wampCRAAuth.signWAMPCRAChallenge(challenge.trim(), Buffer.from(TEST_SECRET));
 
@@ -27,15 +47,55 @@ describe('WAMP CRA Tests', function () {
         );
     });
 
-    it('Verify CRA Signature', async function() {
-        const challenge: string = await runCommand('wampproto auth cra generate-challenge 1 foo admin static');
+    it('Verify CRA Signature', async function () {
+        const challenge: string = await runCommand(
+            `wampproto auth cra generate-challenge ${SESSION_ID} ${AUTH_ID} ${AUTH_ROLE} ${PROVIDER}`
+        );
 
         const signature: string = await runCommand(
             `wampproto auth cra sign-challenge '${challenge.trim()}' ${TEST_SECRET}`
         );
 
         const isVerified: boolean = wampCRAAuth.verifyWAMPCRASignature(
-            signature.trim(), challenge.trim(), Buffer.from(TEST_SECRET)
+            signature.trim(),
+            challenge.trim(),
+            Buffer.from(TEST_SECRET)
+        );
+
+        expect(isVerified).toBeTruthy();
+    });
+
+    it('Sign WAMPCRA Signature With Salt', async function () {
+        const challenge = new Challenge(new ChallengeFields(WAMPCRAAuthenticator.TYPE, AUTH_EXTRA));
+        const authenticator = new WAMPCRAAuthenticator(AUTH_ID, TEST_SECRET, AUTH_EXTRA);
+        const authenticate = authenticator.authenticate(challenge);
+
+        const saltSecret = await runCommand(
+            `wampproto auth cra derive-key ${SALT} ${TEST_SECRET} -i ${ITERATIONS} -l ${KEY_LENGTH}`
+        );
+
+        await runCommand(
+            `wampproto auth cra verify-signature '${CRA_CHALLENGE}' ${authenticate.signature} ${saltSecret.trim()}`
+        );
+    });
+
+    it('Verify WAMPCRA Signature With Salt', async function () {
+        const challenge: string = await runCommand(
+            `wampproto auth cra generate-challenge ${SESSION_ID} ${AUTH_ID} ${AUTH_ROLE} ${PROVIDER}`
+        );
+
+        const saltSecret = await runCommand(
+            `wampproto auth cra derive-key ${SALT} ${TEST_SECRET} -i ${ITERATIONS} -l ${KEY_LENGTH}`
+        );
+
+        const signature = await runCommand(
+            `wampproto auth cra sign-challenge '${challenge.trim()}' ${saltSecret.trim()}`
+        );
+
+        const isVerified: boolean = wampCRAAuth.verifyWAMPCRASignature(
+            signature.trim(),
+            challenge.trim(),
+            Buffer.from(saltSecret.trim())
         );
 
         expect(isVerified).toBeTruthy();
